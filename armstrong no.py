fe864a7c5d38c49f115a32de345ae9592282051f
Abhinav Asthana <143867960+asthana-16@users.noreply.github.com>
2023-12-03 18:50:45 +0530

    Armstrong No.py

--- armstrong no.py
+++ armstrong no.py
@@ -1,18 +1,15 @@
 
-# num = int(input("Enter a number: "))
-# sum = 0
+num = int(input("Enter a number: "))
+sum = 0
 
-# temp = num
-# while temp > 0:
-#    digit = temp % 10
-#    sum += digit ** 3
-#    temp //= 10
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
 
-# if num == sum:
-#    print(num,"is an Armstrong number")
-# else:
-#    print(num,"is not an Armstrong number")
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
 
-a = [0, 1, 2, 3]
-del a[:]
-print(a)
